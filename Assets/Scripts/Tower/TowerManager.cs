4d8d6bc8bbccc4f5d7d12da9a79bff6dfe120f81
Syny <s.raouf@student.isartdigital.com>
2018-02-02 09:56:06 -0500

    Drag and Drop towers - Build towers on empty buildsites

--- Assets/Scripts/Tower/TowerManager.cs
+++ Assets/Scripts/Tower/TowerManager.cs
@@ -4,10 +4,12 @@ using UnityEngine.EventSystems;
 public class TowerManager :Singleton<TowerManager> {
 
     private TowerBtn towerBtnPressed;
+    private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -20,14 +22,25 @@ public class TowerManager :Singleton<TowerManager> {
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero); //Raycast from lower left corner to where we clicked
 
             if(hit.collider.tag == "BuildSite")
+            {
+                hit.collider.tag = "BuildSiteFull";
                 PlaceTower(hit);
+            }
+
+          
 
         }
-	}
+
+        if (spriteRenderer.enabled)
+        {
+            FollowMouse();
+        }
+    }
 
     public void SelectedTower(TowerBtn towerSelected)
     {
         towerBtnPressed = towerSelected;
+        EnableDragSprite(towerBtnPressed.DragSprite);
     }
     
      public void PlaceTower(RaycastHit2D hit)
@@ -37,7 +50,27 @@ public class TowerManager :Singleton<TowerManager> {
             GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
 
             newTower.transform.position = hit.transform.position;
+
+            DisableDragSprite();
         }
+    }
+
+    public void FollowMouse()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Bug you have to reset position to see the drag 
+        transform.position = new Vector2(transform.position.x, transform.position.y );
+    }
+
+    public void EnableDragSprite(Sprite sprite)
+    {
+        spriteRenderer.enabled = true;
+        spriteRenderer.sprite = sprite;
+    }
+
+    public void DisableDragSprite()
+    {
+        spriteRenderer.enabled = false;
     }
 
 }
